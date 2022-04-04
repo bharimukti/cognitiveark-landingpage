8a381d5faf650660cc62fdbf1589a16c058f1afa
Endy Hardy <endy0611@gmail.com>
2022-04-05 00:06:45 +0800

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Valorant", ...defaultTheme.fontFamily.sans],
+        sans: ["Futura", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
