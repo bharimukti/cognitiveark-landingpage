7212a8ac193db6c6378b46566efcc91ee57ce5ed
Endy Hardy <endy0611@gmail.com>
2022-04-05 09:38:01 +0800

    jump to section

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,52 @@
-import { FC, useEffect, useState } from "react";
-import Link from "next/link";
+import { FC, useCallback, useEffect, useState } from "react";
+
+const JUMP_TO_SECTIONS = [
+  { id: "titleSection", label: "Home" },
+  { id: "middleSection", label: "About Us" },
+  { id: "roadmapSection", label: "Roadmap" },
+  { id: "disclaimerSection", label: "Disclaimer" },
+  { id: "teamSection", label: "Team" },
+];
 
 const Nav: FC = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(JUMP_TO_SECTIONS[0].id);
+
+  const handleJumpToSection = useCallback((sectionId) => {
+    const contentContainer = document.getElementById("content");
+    if (!contentContainer) {
+      return;
+    }
+    const sectionToJumpTo = document.getElementById(sectionId);
+    if (!sectionToJumpTo) {
+      return;
+    }
+    contentContainer.scrollTo({
+      top: sectionToJumpTo.offsetTop,
+      behavior: "smooth",
+    });
+  }, []);
 
   useEffect(() => {
     const contentContainer = document.getElementById("content");
     if (contentContainer) {
       const handleWindowScroll = () => {
-        setScrolled((contentContainer.scrollTop || 0) > 50);
+        const scrollDistance = contentContainer.scrollTop || 0;
+        setScrolled(scrollDistance > 50);
+        const currentScrollPosition = JUMP_TO_SECTIONS.reduce(
+          (agg, section) => {
+            const sectionElement = document.getElementById(section.id);
+            if (!sectionElement) {
+              return agg;
+            }
+            if (sectionElement.offsetTop <= scrollDistance) {
+              return section.id;
+            }
+            return agg;
+          },
+          JUMP_TO_SECTIONS[0].id
+        );
+        setScrollPosition(currentScrollPosition);
       };
 
       contentContainer.addEventListener("scroll", handleWindowScroll);
@@ -28,19 +66,25 @@ const Nav: FC = () => {
         <div className="container max-w-screen-sm lg:max-w-screen-md 2xl:max-w-screen-lg mx-auto">
           <nav className="flex justify-center lg:justify-end font-display">
             <ul className="flex items-center space-x-8 py-4">
-              {["Home", "About Us", "Roadmap", "Disclaimer", "Team"].map(
-                (menuItem) => {
-                  return (
-                    <li key={menuItem}>
-                      <Link href="/">
-                        <a className="transition-colors hover:text-primary duration-200 ease-in-out">
-                          {menuItem}
-                        </a>
-                      </Link>
-                    </li>
-                  );
-                }
-              )}
+              {JUMP_TO_SECTIONS.map((section) => {
+                const isActive = scrollPosition === section.id;
+                return (
+                  <li key={section.id}>
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleJumpToSection(section.id);
+                      }}
+                      className={`${
+                        isActive ? "text-primary" : ""
+                      } transition-colors hover:text-primary duration-200 ease-in-out`}
+                    >
+                      {section.label}
+                    </a>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
